6d83f951f6e73806aaa4ebca8c5c3927c9fad833
Todd Poynor <toddpoynor@google.com>
2014-01-03 16:00:28 -0800

    flounder: add dumpstate static HAL
    Includes dumping charger chip register state.
    
    Change-Id: I5f2e8f53bc19e29fecee4a0953f41cfdb7069155

---
+++ dumpstate/dumpstate.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dumpstate.h>
+
+void dumpstate_board()
+{
+    dump_file("soc revision", "/sys/devices/soc0/revision");
+    dump_file("soc die_id", "/sys/devices/soc0/soc_id");
+    dump_file("bq2419x charger regs", "/d/bq2419x-regs");
+    dump_file("shrinkers", "/d/shrinker");
+};
