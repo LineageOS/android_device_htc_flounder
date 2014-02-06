fa1ffccc0fcef5fcde196961c55153b4af603e12
stanly_lin <stanly_lin@htc.com>
2014-02-06 20:40:26 +0800

    flounder: touch fusion: real touch panel for 64bit device.
    Change-Id: I3bd13632369a6ceabc118d0ec4fa4786eed92317
    flounder: device: copy the touch fusion binary.
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- device32.mk
+++ device32.mk
@@ -4,6 +4,12 @@ else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/touch/touch_fusion:system/vendor/bin/touch_fusion \
+    $(LOCAL_PATH)/touch/touch_fusion.cfg:system/vendor/firmware/touch_fusion.cfg \
+    $(LOCAL_PATH)/touch/maxim_fp35.bin:system/vendor/firmware/maxim_fp35.bin \
+    $(LOCAL_PATH)/touch/touch_fusion.idc:system/usr/idc/touch_fusion.idc
+
 $(call inherit-product-if-exists, device/htc/flounder/device.mk)
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra124/tegra124.mk)
