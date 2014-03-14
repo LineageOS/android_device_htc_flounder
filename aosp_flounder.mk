faf0c33d065ae94c31094d0f9e9e3e054edf1f82
Colin Cross <ccross@android.com>
2014-03-14 14:49:35 -0700

    flounder: make aosp_flounder a 64-bit build
    aosp_flounder is now a 64-bit multilib build.  aosp_flounder64 is
    an alias for aosp_flounder.  aosp_flounder32 is a pure 32-bit build.
    
    Change-Id: I909bff1dc67032c1882d273699f7277dfda29a48

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -21,28 +21,10 @@
 # lines, aosp and flounder, hence its name.
 #
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-        LiveWallpapers \
-        LiveWallpapersPicker \
-        MagicSmokeWallpapers \
-        VisualizationWallpapers \
-        librs_jni
-
-PRODUCT_PROPERTY_OVERRIDES := \
-        net.dns1=8.8.8.8 \
-        net.dns2=8.8.4.4
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, device/htc/flounder/device.mk)
-
-PRODUCT_RUNTIMES := runtime_libart_default
-PRODUCT_RUNTIMES += runtime_libdvm
+$(call inherit-product, device/htc/flounder/aosp_flounder64.mk)
 
 PRODUCT_NAME := aosp_flounder
 PRODUCT_DEVICE := flounder
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Flounder
 PRODUCT_MANUFACTURER := nVidia
-PRODUCT_RESTRICT_VENDOR_FILES := owner path
