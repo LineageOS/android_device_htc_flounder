4389f09bdb0f29ca836e7c65a62770456c8d64b5
Lajos Molnar <lajos@google.com>
2014-03-19 14:45:02 -0700

    stagefright: use media_codecs_google*.xml files
    Bug: 13549114
    Change-Id: Ia9d8bdd06f0ab745c04b32fecc7a981679347c4a

--- device.mk
+++ device.mk
@@ -92,6 +92,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
