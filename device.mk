9b0e82358eb9d57c043a0d4814ee19e581eef3bb
Thomas.TT_Lin <Thomas.TT_Lin@htc.com>
2014-05-28 21:27:32 +0800

    flounder: bluetooth: Enable system hardware Bluetooth/Bluetooth_LE feature
    Change-Id: I6bb6c9fd1d3b6d445d58166244a37b4b51dd313d
    Signed-off-by: Thomas.TT_Lin <Thomas.TT_Lin@htc.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- device.mk
+++ device.mk
@@ -76,7 +76,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
