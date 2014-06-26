3113d35948f2f39d5972d26a7af99da7c6a77ac6
JP Abgrall <jpa@google.com>
2014-06-26 16:59:49 -0700

    BoardConfig+device: Don't build a userdata.img, don't use F2FS on non-linux
    A userdata image is not very usefull as it does not match
    the size of the device(s).
    Don't waste time building it.
    
    Also, on non-linux HOST_OS, don't bother with host F2FS tools.
    
    The end result, is that on Mac OS, it will build volantis,
    then fastboot will only erase userdata instead of formatting it,
    and once it boots up it will format the erased paritition.
    
    
    Change-Id: I7f2f6c92ca2af2da3fd741ecaab5d089c279c32b
    Signed-off-by: JP Abgrall <jpa@google.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,11 @@ endif
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 
 # This ensures the needed build tools are available.
+# TODO: make non-linux builds happy with external/f2fs-tool; system/extras/f2fs_utils
+ifeq ($(HOST_OS),linux)
 TARGET_USERIMAGES_USE_F2FS := true
+endif
+
 LOCAL_FSTAB := $(LOCAL_PATH)/fstab.flounder
 
 
