2f5d603ad453a0071e455fa5112c41019d2e1113
Mohamad Ayyash <mkayyash@google.com>
2014-07-11 11:59:18 -0700

    aosp_flounder.mk: Ensure that aosp_base.mk is the last to be inherited.
    Change-Id: I00b90b7306c3cbabdd8db6574c3114be89837d0e

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -31,10 +31,10 @@ PRODUCT_PROPERTY_OVERRIDES := \
         net.dns2=8.8.4.4
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/htc/flounder/product.mk)
 $(call inherit-product, device/htc/flounder/device-lte.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := aosp_flounder
 PRODUCT_DEVICE := flounder
