0a5d06b2ed7b155d81d129e56d142d256f981dbf
tai_kuo <tai_kuo@htc.com>
2014-08-21 21:24:22 +0800

    Flounder: Setting: Show Regulatory info menu in setting page
    Show Regulatory info menu in setting page
    
    Bug: 16882456
    Signed-off-by: tai_kuo <tai_kuo@htc.com>
    Change-Id: I1d06926d8b7f32d8d55e1e3d2ef8d0c06bc943a6

--- device-lte.mk
+++ device-lte.mk
@@ -29,3 +29,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
     telephony.lteOnCdmaDevice=1
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/lte_only_overlay
