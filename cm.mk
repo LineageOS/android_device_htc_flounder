cb68f0196408b229f4271ca5ac2a1df420b49625
Simon Sickle <simon@simonsickle.com>
2015-01-26 18:41:30 -0600

    this is a flounder, not a whale...
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/flounder/aosp_shamu.mk)
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
