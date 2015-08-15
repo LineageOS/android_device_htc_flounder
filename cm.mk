4c67d8fce476b15df6c7dd31f5b4195956150a15
Simon Sickle <simon@simonsickle.com>
2015-08-15 16:52:25 -0700

    Update flounder to 5.1.1_r13
    Change-Id: Ib2bdb8de515736042c71012ce58f1d1844737628
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- cm.mk
+++ cm.mk
@@ -22,6 +22,7 @@ TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+TARGET_PREBUILT_KERNEL := false
 
 # Extra Packages
 PRODUCT_PACKAGES += \
@@ -30,10 +31,14 @@ PRODUCT_PACKAGES += \
 # CM Overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
+# Enable USB OTG (CAF commit to Settings)
+ADDITIONAL_BUILD_PROPERTIES += \
+    persist.sys.isUsbOtgEnabled=true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY47X/1849464:user/release-keys \
-    PRIVATE_BUILD_DESC="flounder-user 5.1.1 LMY47X 1849464 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY48I 2074855 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_flounder
--- device.mk
+++ device.mk
@@ -20,6 +20,16 @@ PRODUCT_PACKAGES := \
     wpa_supplicant \
     wpa_supplicant.conf
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+ifeq ($(USE_SVELTE_KERNEL), true)
+LOCAL_KERNEL := device/htc/flounder_svelte-kernel/Image.gz-dtb
+else
+LOCAL_KERNEL := device/htc/flounder-kernel/Image.gz-dtb
+endif # USE_SVELTE_KERNEL
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 
 # This ensures the needed build tools are available.
@@ -236,14 +246,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.enable_boot_charger_mode=1
 
-# Enable USB OTG (CAF commit to Settings)
-ADDITIONAL_BUILD_PROPERTIES += \
-    persist.sys.isUsbOtgEnabled=true
-
 # add verity dependencies
-$(call inherit-product, build/target/product/verity.mk)
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/APP
 PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
+$(call inherit-product, build/target/product/verity.mk)
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
 $(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
