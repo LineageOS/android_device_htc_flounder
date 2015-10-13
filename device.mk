aa7905d2a1d5a1efc96e23e976959fac4f9b67c4
beanstown106 <nbenis106@gmail.com>
2015-10-13 06:30:13 -0400

    Kill device image verification completely
    Change-Id: I773e2b90c90ef2cafa03038ad642af0e97f5e6b1

--- device.mk
+++ device.mk
@@ -265,18 +265,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio_hal.period_size=128
 
-# add verity dependencies
-$(call inherit-product, build/target/product/verity.mk)
-PRODUCT_SUPPORTS_BOOT_SIGNER := false
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/APP
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
-$(call inherit-product, build/target/product/verity.mk)
-
-# for warning
-PRODUCT_PACKAGES += \
-    slideshow \
-    verity_warning_images
-
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
 $(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
