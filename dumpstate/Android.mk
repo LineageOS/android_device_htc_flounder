1ef2d9feeaf70605c04884f61205bd2ac8815574
Felipe Leme <felipeal@google.com>
2015-11-10 10:29:59 -0800

    Migrated dumpstate to C++. BUG: 25563823
    Change-Id: I9efb6dc437d559d238cb8ba58ed6d5108c7226ff

--- dumpstate/Android.mk
+++ dumpstate/Android.mk
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
 
-LOCAL_SRC_FILES := dumpstate.c
+LOCAL_SRC_FILES := dumpstate.cpp
 
 ifneq ($(wildcard vendor/htc/flounder/uidcheck/Android.mk),)
 LOCAL_CFLAGS += -DHAS_DENVER_UID_CHECK
