e33dc779d4387329cc5f004dc5528b11d002689a
Joshndroid <fmxjoshiewah@gmail.com>
2016-03-11 21:45:34 +1000

    Flounder : Update build fingerprint to MMB29V
    Change-Id: I0cb8c2ce59ddc090d86398297827eb25d44650f1

--- cm.mk
+++ cm.mk
@@ -39,9 +39,9 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys" \
-    BUILD_ID=MMB29R
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29V/2554798:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29V 2554798 release-keys" \
+    BUILD_ID=MMB29V
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_flounder
