bf401fa6e512a1b676de3c4a02662db3642fcc3c
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-31 16:25:08 -0700

    Verity on user builds only
    Change-Id: Id1825adbfd9b2ffcde98db77602686e504ba3c1b

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio_hal.period_size=128
 
+# only include verity on user builds for CM
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    PRODUCT_COPY_FILES += device/htc/flounder/fstab-verity.flounder:root/fstab.flounder
+
 # add verity dependencies
 $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_SUPPORTS_BOOT_SIGNER := false
@@ -271,6 +275,7 @@ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/APP
 PRODUCT_PACKAGES += \
     slideshow \
     verity_warning_images
+endif
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
 $(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
