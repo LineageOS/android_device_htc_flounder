b1fc33c890977f58ac322642b5bd42fd40e29ac9
Ruchi Kandoi <kandoiruchi@google.com>
2016-10-07 12:35:33 -0700

    flounder: device.mk: Add hidl product packages for memtrack
    Bug: 31180823
    Change-Id: I83baf38876c8f65bb56a2a79e8cecaee3c10288e
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

--- device.mk
+++ device.mk
@@ -226,6 +226,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.flounder
 
+# Memtrack HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
