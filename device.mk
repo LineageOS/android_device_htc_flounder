fc681984aac236b3d08a435d92ee9682ee8c469d
Prashant Malani <pmalani@google.com>
2016-10-07 18:28:21 -0700

    flounder: Add Vibrator default impl to device build
    Bug: 32021191
    Change-Id: Ia838585ca4952d5e706552848254cb05fc969f0e
    Test: m

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     thermal.flounder
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     fsck.f2fs mkfs.f2fs
