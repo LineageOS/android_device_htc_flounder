0f049bb9016b1434d6cc768ad348d10841c349e3
Mikhail Naganov <mnaganov@google.com>
2016-11-21 15:22:17 -0800

    flounder: Add all audio HIDL *-impl libraries to the list of product packages
    As *-impl.so files are loaded via dlopen, they don't get added
    automatically with the audio HAL server.
    
    Test: check files on the device
    Change-Id: I9ec8e0b7883d88f3077fcb425a33f19162b2f1df

--- device.mk
+++ device.mk
@@ -173,6 +173,13 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Audio HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     libbt-vendor \
