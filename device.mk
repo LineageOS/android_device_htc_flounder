88c3acd682356211a12b0c1ea39fdb83e43c9cec
Mark Salyzyn <salyzyn@google.com>
2017-01-06 08:50:56 -0800

    flounder: limit dex2oat to one thread
    Denver processor only has 2 cpus, we should limit dex2oat
    background operations to 1 thread and the associated lower
    memory footprint so that the impact is controlled.
    
    Bug: 34114061
    Test: manual smoke test
    Change-Id: I1f32b1734aad5765223959a9735a66b086e3448a

--- device.mk
+++ device.mk
@@ -226,6 +226,9 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
+# N9 has only two CPUs, so drop dex2oat pressure after boot at runtime
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=1
 
 # for off charging mode
 PRODUCT_PACKAGES += \
