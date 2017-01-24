628021f0dd2d3bba91bda175265d0b2fa620039d
Badhri Jagan Sridharan <Badhri@google.com>
2017-01-23 18:17:59 -0800

    flounder: Add usb hal service
    BUG: 31015010
    Test: Manually tested on Angler for type-c role switches.
    Change-Id: I483776f3d336c38c6573eaf8e06d2b5755a2dc5d

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     thermal.flounder
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
