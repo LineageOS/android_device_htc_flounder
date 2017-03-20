effbd72587389d705206624c3351770966db360f
Janis Danisevskis <jdanis@google.com>
2017-03-20 08:00:54 -0700

    flounder: Make gatekeeper binderized always
    This patch adds the gatekeeper-service package to
    device/google/flounder/device.mk such that the
    binderized gatekeeper hal service gets build.
    
    In addition the HAL transport type for android.hardware.gatekeeper
    is set to "hwbinder".
    
    Bug: 34260418
    Test: Boot the device and check the output of logcat. gatekeeperd must
          not fall back to software gatekeeper.
    
    Change-Id: Icd0963943c35dbac3b29588ae79f03f14b67729a

--- device.mk
+++ device.mk
@@ -203,7 +203,8 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
