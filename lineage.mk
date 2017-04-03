164cc56d23be159bc31f62017aad966f91e3f234
Hashbang173 <hashbang173@gmail.com>
2017-04-03 20:01:36 +0000

    flounder: update fingerprint to N4F26X
    Change-Id: I4c47b4a189799c58e3577149e2b2042e1c8bc46e

--- lineage.mk
+++ lineage.mk
@@ -39,8 +39,8 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26T/3687331:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26T 3687331 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26X/3821951:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26X 3821951 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_flounder
