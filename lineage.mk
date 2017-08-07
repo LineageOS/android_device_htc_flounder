5755028ec759e3e2821c0d00e19cf311e40bd9ab
razorloves <razorloves@gmail.com>
2017-08-07 14:20:46 -0500

    flounder: update build fingerprint & description to N9F27H
    Change-Id: Ia9fe80a5e67f5b5aaac191247b354eb65e0d527c

--- lineage.mk
+++ lineage.mk
@@ -39,8 +39,8 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27F/4072790:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N9F27F 4072790 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27H/4108833:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N9F27H 4108833 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_flounder
