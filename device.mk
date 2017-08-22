93107c5ab607940fb4be8c1826c5bc72de4d79dd
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    flounder: Build Vibrator HAL
    Change-Id: I39aa24d469f74e2279c40bb643c7397f1086e48c

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # for launcher layout
 #PRODUCT_PACKAGES += \
 #    VolantisLayout
