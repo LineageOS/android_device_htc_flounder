d683f42fe5e573e2c1551e9be38d05d78cc8c39e
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:12 +0200

    flounder: Build GNSS HAL
    Change-Id: I7351c7eed3741d2e79ee2f8923091f08540f5e84

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
 	VolantisKeyboard
 
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # Gralloc HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
