240c721f413bde6e3c82ccace93488c6341d49c6
Bruno Martins <bgcngm@gmail.com>
2017-12-15 00:45:54 +0000

    flounder: Properly support Widevine DRM
    Change-Id: If7e6f4e25deb4aaba2b7f10554fb8251c5e08b50

--- device.mk
+++ device.mk
@@ -184,7 +184,8 @@ PRODUCT_PACKAGES += \
 
 # DRM HAL
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
