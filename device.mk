eaaad04c9086b5cd497fc0642baa7ca64388e348
Bruno Martins <bgcngm@gmail.com>
2018-03-27 12:25:22 +0100

    flounder: Binderize health HAL
    Change-Id: If2033da5556ffaab5c431e63ab10c673a5951d39

--- device.mk
+++ device.mk
@@ -218,7 +218,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
