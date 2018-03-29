2c9d58740512b949704acc33fcf0fc2ac357a6dd
Bruno Martins <bgcngm@gmail.com>
2018-03-29 12:29:41 +0100

    flounder: Remove useless AOSP makefiles
     * Building aosp_flounder target should still be possible
    
    Change-Id: I94306871043b5d0555172f3bd1bc2820b272e204

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_flounder.mk \
-	$(LOCAL_DIR)/aosp_flounder64.mk \
-	$(LOCAL_DIR)/aosp_flounder32.mk \
-	$(LOCAL_DIR)/aosp_flounder_64_only.mk
+    $(LOCAL_DIR)/aosp_flounder.mk
