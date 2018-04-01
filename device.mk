24965d3bc08f86a279601fa6abd5e0ef6f2b02e8
Bruno Martins <bgcngm@gmail.com>
2018-04-01 11:42:05 +0100

    flounder: Completely ditch HWC2
     * Prebuilt hwcomposer 1.2 is being used and is actually the
       only one working properly (refer to commit 8246d04).
    
    Change-Id: Ie9a057286a61e4ecc226cd162c46dde204b4a7f1

--- device.mk
+++ device.mk
@@ -195,7 +195,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     hwcomposer.flounder
 
