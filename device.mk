e765d63d6efaa4022630a39ce316ea5a7ced631d
Hashbang173 <hashbang173@gmail.com>
2018-08-08 14:34:25 -0400

    flounder: Enable HWC2on1 adapter
     * This fixes display (see: https://android.googlesource.com/platform/frameworks/native/+/d265c4238699ea899cc3b01cc90b643d9692db2a)
    
    Change-Id: Ic749a38828563be7dacd005aeb91dbce3121ae07

--- device.mk
+++ device.mk
@@ -193,6 +193,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     hwcomposer.flounder
 
