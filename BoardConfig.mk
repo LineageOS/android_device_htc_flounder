38d1c79f7059558c4cdee38c4cabb7a4e699eac7
Hashbang173 <hashbang173@gmail.com>
2018-08-08 13:55:33 -0400

    flounder: Remove USE_CLANG_PLATFORM_BUILD
    Change-Id: Id8a0a2a0a4c92f8cdee1e5532df4f600ee21f7c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,8 +127,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 MALLOC_SVELTE := true
 
-USE_CLANG_PLATFORM_BUILD := true
-
 # Forcebly use the non-open-source parts
 include vendor/htc/flounder-common/BoardConfigVendor.mk
 ifeq ($(TARGET_PRODUCT),lineage_flounder_lte)
