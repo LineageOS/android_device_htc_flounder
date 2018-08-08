57664039debd86ab6c159ff8de18b6c8953a09f8
Hashbang173 <hashbang173@gmail.com>
2018-08-08 13:48:28 -0400

    flounder: Define LOCAL_SDK_VERSION for VolantisKeyboard
    Change-Id: Ia0644606c302d6402fbd7bf03a2f3d35bb79bb05

--- VolantisKeyboard/Android.mk
+++ VolantisKeyboard/Android.mk
@@ -20,6 +20,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := VolantisKeyboard
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 
