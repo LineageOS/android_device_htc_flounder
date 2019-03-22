26fdc111ec282be0a40a4db571d65633bf542f51
Vladimir Oltean <olteanv@gmail.com>
2019-03-22 22:37:05 +0000

    flounder: Sync extractors with templates
     * This introduces fixup scripts and kang mode and fixes
       passing of arguments longer than one word,
       such as --section "Hello world".
    
    Change-Id: I42dde659e9bea4f7e1c9a7a3fa8da3243774ca4e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 
 set -e
 
-if [ -z "$DEVICE" ]; then
+if [ -z "${DEVICE}" ]; then
     DEVICE=flounder
 fi
 DEVICE_COMMON=flounder-common
@@ -28,38 +28,38 @@ INITIAL_COPYRIGHT_YEAR=2016
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and common guards
 write_headers "flounder flounder_lte"
 
 # Generate Treble compatible makefiles
-write_makefiles "$MY_DIR"/common-proprietary-files.txt true
+write_makefiles "${MY_DIR}/common-proprietary-files.txt" true
 
 write_footers
 
-if [ -s "$MY_DIR"/proprietary-files.txt ] || [ -s "$MY_DIR"/../$DEVICE/device-proprietary-files.txt ]; then
+if [ -s "${MY_DIR}/proprietary-files.txt" ] || [ -s "${MY_DIR}/../${DEVICE}/device-proprietary-files.txt" ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
 
     # Copyright headers and guards
     write_headers
 
     # Generate Treble compatible makefiles
-    write_makefiles "$MY_DIR"/proprietary-files.txt true
-    write_makefiles "$MY_DIR"/../$DEVICE/device-proprietary-files.txt true
+    write_makefiles "${MY_DIR}/proprietary-files.txt" true
+    write_makefiles "${MY_DIR}/../${DEVICE}/device-proprietary-files.txt" true
 
     write_footers
 fi
